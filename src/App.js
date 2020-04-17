253c57402f0b98d01d42b57f54921af04d9df69f
AbishekBee <abishekdongol@beetechsolution.com>
2020-04-17 19:21:00 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/master/Header";
+import Menu from "./components/master/Menu";
+import Footer from "./components/master/Footer";
+import Content from "./components/master/Content";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
     </div>
   );
 }
-
-export default App;
